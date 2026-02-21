a70ccad7dd6d48e7d17e733b59349f9bf9d79e0c
Luiz Filipe Ferreira Gonçalves <luisfhylipe@gmail.com>
2026-02-21 18:37:59 -0300

    edits

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHoraria(15);
+
+        Mentoria  mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria java");
+        mentoria1.setDescricao("Descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1.toString());
+        System.out.println(curso2.toString());
+        System.out.println(mentoria1.toString());
+    }
+}
